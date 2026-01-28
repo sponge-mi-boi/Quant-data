8d696fd372250f5c8f585262106b63f352c56845
Varun <varunkannan2000@gmail.com>
2026-01-28 12:41:45 +0530

    Version 0- Foundations Part 2.1

--- src/Quant_Bot/Market_Analysis.py
+++ src/Quant_Bot/Market_Analysis.py
@@ -18,19 +18,21 @@ pd.set_option('display.max_column', None)
 pd.set_option('display.width', 0)
 np.set_printoptions(threshold=np.inf)
 
-
+# These are the custom keys for the user's Alpaca account.
 API_KEY = ''
 SECRET_KEY = ''
-BASE_URL = 'https://paper-api.alpaca.markets'
 
-client = StockHistoricalDataClient(api_key=API_KEY,secret_key=SECRET_KEY)
+BASE_URL = 'https://paper-api.alpaca.markets'
 
 # List of stock pairs needed to get the data
 ## This should be either customized in terms of which stocks to get the data of
-full_stocks = os.listdir('Extras/Stocks')
+full_stocks = []
 
-#shift
-def get_time_period( args, custom_data = False, num_data_points = 100,freq='d',details=False,shift =1):
+# Used to dynamically obtain the data of a stock given parameters such as the number of points, which can be based on
+#either custom, already obtained data, or dynamically obtained from the alpaca website.
+## 'shift' is defined from the starting point of the data.
+## Live data is obtained only through an approximation of the needed number of data points, meaning it is not exact.
+def get_time_period( args, custom_data = False, num_data_points = 100,freq='d', shift =1) -> pd.DataFrame:
 
     if custom_data:
         if freq == 'd':
@@ -45,31 +47,31 @@ def get_time_period( args, custom_data = False, num_data_points = 100,freq='d',d
             data = pd.read_parquet('Closemax1h.parquet').iloc[shift:num_data_points + shift][args].dropna()
 
         else:
-            return None
+            return pd.DataFrame()
     else:
         if freq == 'd':
-            tm = datetime.datetime.now() - datetime.timedelta(days =num_data_points)*5
+            tm_start = datetime.datetime.now() - datetime.timedelta(days =num_data_points)*5
             timeframe = TimeFrame.Day
         elif freq == 'h':
             timeframe = TimeFrame.Hour
-            tm = datetime.datetime.now() - datetime.timedelta(hours =num_data_points)*10
+            tm_start = datetime.datetime.now() - datetime.timedelta(hours =num_data_points)*10
 
         elif freq == '5m':
             timeframe = TimeFrame(5,TimeFrameUnit.Minute)
-            tm = datetime.datetime.now() - datetime.timedelta(minutes =5 * num_data_points)*1000
+            tm_start = datetime.datetime.now() - datetime.timedelta(minutes =5 * num_data_points)*1000
 
         elif freq == '15m':
             timeframe = TimeFrame(15,TimeFrameUnit.Minute)
-            tm = datetime.datetime.now() - datetime.timedelta(minutes = 15 * num_data_points)*100
+            tm_start = datetime.datetime.now() - datetime.timedelta(minutes = 15 * num_data_points)*100
         elif freq == '30m':
             timeframe = TimeFrame(15 * 2, TimeFrameUnit.Minute)
-            tm = datetime.datetime.now() - datetime.timedelta(minutes=15 *( 2 + 1 -1 ) * num_data_points)*1000
+            tm_start = datetime.datetime.now() - datetime.timedelta(minutes=15 *( 2 + 1 -1 ) * num_data_points)*1000
         else:
-            return None
+            return pd.DataFrame()
         data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
         stock_bars = data_client.get_stock_bars(
-            request_params=StockBarsRequest(timeframe=timeframe, symbol_or_symbols=args, start=tm)).df['close'].reset_index()
+            request_params=StockBarsRequest(timeframe=timeframe, symbol_or_symbols=args, start=tm_start)).df['close'].reset_index()
         data = pd.DataFrame()
         for x in args:
             close = stock_bars[stock_bars['symbol'] == x]
@@ -80,8 +82,9 @@ def get_time_period( args, custom_data = False, num_data_points = 100,freq='d',d
                 data[x] = pd.Series(index=list(close['timestamp']),data=list(close['close']))
     return data
 
+# A wrapper on the yahoo finance website for easy creation and storage of new data.
 def get_yf(per,intr,stocks=tuple(full_stocks)) -> None:
-    t = yfinance.download(period=per,interval= intr,tickers=stocks + ['SPY'])['Close']
+    t = yfinance.download(period=per,interval= intr,tickers=list(stocks) + ['SPY'])['Close']
     results = (t.isna().sum() > int(1/1000*len(t.index)))
     t[[x for x in results.index if not results[x]]].to_parquet('Close' + per + intr + '.parquet')
     return
